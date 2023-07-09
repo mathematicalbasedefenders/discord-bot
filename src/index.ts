bad4b6c2ce40222584bce618b74592e264ff3429
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-07-09 20:37:05 +0700

    change: more stuff

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
+import fs from "fs";
+import path from "path";
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import { log } from "./log";
 require("dotenv").config({});
+
+const rawConfig = fs.readFileSync(
+	path.join(__dirname, "..", "configuration.json")
+);
+const configuration = JSON.parse(rawConfig.toString());
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, (c: any) => {
-	log.info(`Logged in as ${c.user.tag}`);
+	log.info(
+		`Logged in as ${c.user.tag} with base url ${configuration.baseURL}`
+	);
 });
 
 client.login(process.env.DISCORD_TOKEN);
