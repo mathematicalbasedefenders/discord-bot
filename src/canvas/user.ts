762e69420acb0583544f5b13ff77b576f1668767
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-03-18 22:14:26 +0700

    change: moved variables around + use constants for positions

--- src/canvas/user.ts
+++ src/canvas/user.ts
@@ -11,7 +11,18 @@ import { log } from "../log";
 const CANVAS_WIDTH = 1080;
 const CANVAS_HEIGHT = 1080;
 
+const SPACING = 32;
+
+const SECTION_BOX_HEIGHT = 200;
+const SECTION_BOX_WIDTH = CANVAS_WIDTH - 2 * SPACING;
+
+const NOTO_SANS_24 = "24px Noto Sans";
+const NOTO_SANS_72 = "72px Noto Sans";
+const BLACK = "#000000";
+
 const DEFAULT_AVATAR = __dirname + "/assets/default-avatar.png";
+const AVATAR_WIDTH = 256;
+const AVATAR_HEIGHT = 256;
 
 async function getUserStatisticsCanvas(data: any) {
   const fileID = `${data.username}-${Date.now()}`;
@@ -32,23 +43,25 @@ async function createUserStatisticsCanvas(data: unknown, fileName: string) {
     family: "Noto Sans"
   });
 
-  const NOTO_SANS_24 = "24px Noto Sans";
-  const BLACK = "#000000";
-
   createBackground(ctx);
 
   console.log(DEFAULT_AVATAR);
 
   /** Insert avatar here. */
   const defaultAvatar = await loadImage(DEFAULT_AVATAR);
-  ctx.drawImage(defaultAvatar, 32, 32, 256, 256);
+  ctx.drawImage(defaultAvatar, SPACING, SPACING, AVATAR_WIDTH, AVATAR_HEIGHT);
 
+  /** Insert rank here. */
+
+  /** Insert username here.
+   * TODO: Colored names for ranks
+   */
   writeText(ctx, {
     text: JSON.stringify(data),
-    font: NOTO_SANS_24,
+    font: NOTO_SANS_72,
     color: BLACK,
-    x: 500,
-    y: 500
+    x: SPACING * 2 + AVATAR_WIDTH,
+    y: AVATAR_HEIGHT / 2
   });
 
   fs.writeFileSync(fileName, canvas.toBuffer());
