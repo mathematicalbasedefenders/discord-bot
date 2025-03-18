3771bd7a36f938762b4d5e84b08581c94dfb35af
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-03-18 21:56:37 +0700

    change: remove magic number

--- src/canvas/user.ts
+++ src/canvas/user.ts
@@ -3,6 +3,9 @@ import fs from "fs";
 import path from "path";
 import { log } from "../log";
 
+const WIDTH = 1080;
+const HEIGHT = 1080;
+
 function getUserStatisticsCanvas(data: any) {
   const fileID = `${data.username}-${Date.now()}`;
   const fileName = path.join(
@@ -16,7 +19,7 @@ function getUserStatisticsCanvas(data: any) {
 }
 
 function createUserStatisticsCanvas(data: unknown, fileName: string) {
-  const canvas = createCanvas(1024, 1024);
+  const canvas = createCanvas(WIDTH, HEIGHT);
   const ctx = canvas.getContext("2d");
   registerFont(__dirname + "/assets/NotoSans-Regular.ttf", {
     family: "Noto Sans"
@@ -40,7 +43,7 @@ function createUserStatisticsCanvas(data: unknown, fileName: string) {
 
 function createBackground(ctx: CanvasRenderingContext2D) {
   ctx.fillStyle = "#eeeeee";
-  ctx.fillRect(0, 0, 1024, 1024);
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
 }
 
 function writeText(
