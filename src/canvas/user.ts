83f897198b836586f15ded65fb1cd5308bee8bab
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-03-19 19:58:10 +0700

    refactor: move ms`toTime` function to its own file

--- src/canvas/user.ts
+++ src/canvas/user.ts
@@ -520,18 +520,4 @@ function getMultiplayerDataText(data: UserInterface) {
   return result;
 }
 
-function millisecondsToTime(milliseconds: number) {
-  if (!Number.isFinite(milliseconds) || milliseconds < 0) {
-    log.warn("Number given is not a positive finite number.");
-  }
-  let m = Math.floor(milliseconds / 60000);
-  let s = Math.floor((milliseconds % 60000) / 1000)
-    .toString()
-    .padStart(2, "0");
-  let ms = Math.floor((milliseconds % 60000) % 1000)
-    .toString()
-    .padStart(3, "0");
-  return `${m}:${s}.${ms}`;
-}
-
 export { getUserStatisticsCanvas };
