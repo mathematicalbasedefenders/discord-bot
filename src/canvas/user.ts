cc25c44305bde3379fe89d86b4c9baabf5cbc1bd
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-03-19 19:58:45 +0700

    change: put `formatDate` to its own file

--- src/canvas/user.ts
+++ src/canvas/user.ts
@@ -458,15 +458,6 @@ function getLevel(experiencePoints: number | undefined) {
   };
 }
 
-function formatDate(ISODate: string) {
-  const dateObject = new Date(ISODate);
-  return dateObject.toLocaleDateString("en-US", {
-    month: "long",
-    day: "numeric",
-    year: "numeric"
-  });
-}
-
 function getSingleplayerGameDataText(
   data: UserInterface,
   mode: "easy" | "standard"
---
+++ src/utilities/date-formatting.ts
@@ -0,0 +1,8 @@
+function formatDate(ISODate: string) {
+  const dateObject = new Date(ISODate);
+  return dateObject.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  });
+}
